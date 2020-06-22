930d0f6e1e7d9c206e7aa469699b4f8d47fa6fb0
Stefan Fleck <stefan.fleck@statistik.gv.at>
2020-06-22 16:20:20 +0200

    moved AppenderSyslog to this package

--- R/utils-test.R
+++ R/utils-test.R
@@ -39,3 +39,17 @@ dbRemoveTableCaseInsensitive <- function(
 
   res
 }
+
+
+
+
+file_is_readable <- function(x){
+  suppressWarnings(
+  tryCatch({
+    readBin(x, "raw", n = 1)
+    TRUE
+  },
+  error = function(e) FALSE
+  ))
+}
+
